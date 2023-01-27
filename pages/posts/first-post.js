5c522838848520009a5bb344aaade2408993935c
jaussems <jannesaussems@gmail.com>
2023-01-27 13:09:09 +0100

    added HEAD React Component to the First Post Page

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,14 @@
 import Link from 'next/link';
+import Head from 'next/head';
+
 import ImageComponent from '../../components/image';
 
 export default function FirstPost() {
     return (
         <>
+            <Head>
+                <title>First Post</title>
+            </Head>
             <h1>First Post</h1>
             <ImageComponent></ImageComponent>
             <h2>
