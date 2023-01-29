5c7665a5ca13b60aa64aca8bb6c2724a7990e1b2
jaussems <jannesaussems@gmail.com>
2023-01-29 16:59:18 +0100

    added posts in file md format to analyze and understand getStaticProps function and data-fetching aspect using NextJs

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
+import { getSortedPostsData } from '../lib/posts';
 import utilStyles from '../styles/utils.module.css';
 
-export default function Home() {
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+
   return (
     <Layout home>
       <Head>
@@ -15,6 +27,20 @@ export default function Home() {
           This is a introduction example Site to get a grasp of what NextJs is about.
         </p>
         Go to my first <Link href="/posts/first-post">post</Link>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
