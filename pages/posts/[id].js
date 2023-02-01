d19486dbb0ec0a052b50664959d87bef44d99818
jaussems <jannesaussems@gmail.com>
2023-02-01 18:17:41 +0100

    added Styling

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,7 @@
 import Layout from '../../components/layout';
 import Date from '../../components/date';
 import Head from 'next/head';
+import utilStyles from '../../styles/utils.module.css';
 
 
 
@@ -32,12 +33,13 @@ export default function Post({ postData }) {
                 {postData.title}
 
             </Head>
-            <br />
-            {postData.id}
-            <br />
-            <Date dateString={postData.date} />
-            <br />
-            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
         </Layout>
     );
 }
