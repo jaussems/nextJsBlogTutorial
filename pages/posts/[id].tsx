238032ba5c47c2d227a56c041510a13e602044f5
jaussems <jannesaussems@gmail.com>
2023-02-03 20:33:54 +0100

    changed files to TypeScript

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,46 @@
+import Layout from "../../components/layout";
+import Date from "../../components/date";
+import Head from "next/head";
+import utilStyles from "../../styles/utils.module.css";
+
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import { GetStaticPaths, GetStaticProps } from "next";
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params?.id as string);
+  return {
+    props: {
+      postData,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+};
+export default function Post({
+  postData,
+}: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) {
+  return (
+    <Layout>
+      <Head>{postData.title}</Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
